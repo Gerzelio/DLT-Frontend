f141543735354017c240631a39da32aabda18d2b
Gerzélio Saide <gerzeliodomingos@gmail.com>
2022-02-15 14:21:58 +0200

    Refactor HomeNavigator and remove Profile/Refer

--- src/routes/HomeNavigator.tsx
+++ src/routes/HomeNavigator.tsx
@@ -1,14 +1,12 @@
 import React from 'react';
 import {createStackNavigator} from '@react-navigation/stack';
-import Profile from "../views/Profile";
-import Refer from "../views/Refer";
+import Users from "../views/Users";
 
 const HomeStack = createStackNavigator();
 const HomeNavigator: React.FC = () => (
     
-    <HomeStack.Navigator initialRouteName="Profile">
-        <HomeStack.Screen name="Profile" component={Profile} />
-        <HomeStack.Screen name="Refer" component={Refer} />
+    <HomeStack.Navigator initialRouteName="Users">
+        <HomeStack.Screen name="Users" component={Users} />
     </HomeStack.Navigator>
 );
 
