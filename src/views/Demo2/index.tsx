5dc2afb2aac0654062c74c6fe1e05c29151bc4ef
Gerzelio Domingos Saide <gerzeliodomingos@gmail.com>
2022-02-23 11:34:39 +0200

    Add SwipeListView for User form.

---
+++ src/views/Demo2/index.tsx
@@ -0,0 +1,43 @@
+import React, { useState } from 'react';
+import {
+    Dimensions,
+    StyleSheet,
+    Text,
+    TouchableOpacity,
+    View,
+} from 'react-native';
+
+import Basic from './examples/basic';
+
+export default function Demo2() {
+    
+    return (
+        <View style={styles.container}>
+            <View style={styles.switchContainer}>
+                <Text>Teste do switch</Text>
+            </View>
+            {Basic()}
+        </View>
+    );
+}
+
+const styles = StyleSheet.create({
+    container: {
+        backgroundColor: 'white',
+        flex: 1,
+    },
+    switchContainer: {
+        flexDirection: 'row',
+        justifyContent: 'center',
+        marginVertical: 50,
+        flexWrap: 'wrap',
+    },
+    switch: {
+        alignItems: 'center',
+        borderWidth: 1,
+        borderColor: 'black',
+        marginVertical: 2,
+        paddingVertical: 10,
+        width: Dimensions.get('window').width / 3,
+    },
+});
