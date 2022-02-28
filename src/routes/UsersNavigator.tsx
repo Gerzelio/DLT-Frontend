bc2026527e806b3e3e4837987b72a9ae97b30244
Gerzelio Domingos Saide <gerzeliodomingos@gmail.com>
2022-02-28 17:26:22 +0200

    User View

--- src/routes/UsersNavigator.tsx
+++ src/routes/UsersNavigator.tsx
@@ -3,6 +3,7 @@ import {Text} from 'react-native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import UsersListScreen from "../views/Users";
 import UsersFormScreen from "../views/Users/components/create";
+import UsersViewScreen from "../views/Users/components/view";
 
 const UserStack = createNativeStackNavigator();
 
@@ -11,6 +12,14 @@ const UsersNavigator: React.FC = () => {
       <UserStack.Navigator initialRouteName="UserList"  screenOptions={{headerShown:false}}>
         <UserStack.Screen name="UserList" component={UsersListScreen}  />
         <UserStack.Screen name="UserForm" component={UsersFormScreen} options={{
+                    headerTitle: (props) => (
+                      <Text {...props} style={{ color: 'black', fontWeight: 'bold' }}>
+                        back
+                      </Text>
+                    ),
+                    headerShown:true
+                }}/>
+        <UserStack.Screen name="UserView" component={UsersViewScreen} options={{
                     headerTitle: (props) => (
                       <Text {...props} style={{ color: 'black', fontWeight: 'bold' }}>
                         back
